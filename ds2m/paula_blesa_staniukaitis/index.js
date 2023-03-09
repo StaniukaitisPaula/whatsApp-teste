72f12f7bd02226439e23da7efbaf89047ab32d7a
PAULA BLESA STANIUKAITIS <22282124@CFP127ALUNO.LOCAL>
2023-03-09 11:17:10 -0300

    indo

--- ds2m/paula_blesa_staniukaitis/index.js
+++ ds2m/paula_blesa_staniukaitis/index.js
@@ -3,7 +3,7 @@
 import { contatos } from "./contatos.js"
 
 const criarCard = (contato) => {
-    const card = document.createElement('div')
+    const card = document.createElement('button')
     card.classList.add('block')
 
     const img = document.createElement('img')
@@ -21,13 +21,49 @@ const criarCard = (contato) => {
     card.append(img, sender, descricao)
 
     return card
-}
 
+}
 const carregarContatos = () => {
     const chatList = document.getElementById('chatList')
     const cards = contatos.map(criarCard)
 
-    chatList.append(...cards)
+    chatList.replaceChildren(...cards)
+
+
+    cards.forEach(contato => {
+
+        contato.onclick = () => {
+
+            function puxarContato() {
+                document.getElementById('main').style.display = "grid";
+
+
+                const mainHeader = document.createElement('div')
+                mainHeader.classList.add('main__header')
+
+
+                const img = document.createElement('img')
+                img.classList.add('card_image')
+                img.src = `./img/${contato.image}`
+
+                const sender = document.createElement('h2')
+                sender.classList.add('sender')
+                sender.textContent = contato.name
+
+                const descricao = document.createElement('p')
+                descricao.classList.add('description')
+                descricao.textContent = contato.description
+
+
+            }
+
+            puxarContato()
+
+        }
+
+
+
+    });
 }
 
 carregarContatos()
